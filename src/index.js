ac0a536e0570936bcffdbfa09231091134d08325
Roman S <romulus.hund@gmail.com>
2018-11-30 20:33:35 +0200

    MobX has been added

--- src/index.js
+++ src/index.js
@@ -3,9 +3,13 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import bootstrapper from './bootstrapper';
+import {Provider} from "mobx-react";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const injectables = bootstrapper()
+console.log('injectables: ', injectables)
 
+ReactDOM.render(<Provider {...injectables}><App /></Provider>, document.getElementById('root'));
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: http://bit.ly/CRA-PWA
